43a6d76264f5962974daf2523419c6bc4fae6dce
Dzhalal <djaldoit@gmai.com>
2024-05-26 21:49:37 +0300

    high-level-step-objects1

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -1,18 +1,28 @@
-import time
-from selene import have, browser
+from selene import have
 from page.registration_page import RegistrationPage
 
 page = RegistrationPage()
-Piter = page.user_registration('Piter', 'Parker', 'spiderman@example.com', 'Male', '1234567890', 28, 'May', '1995',
-                               'Reading', './images/111.jpg', 'Vladimir Street 12', 'Haryana', 'Karnal')
+user1 = page.user_registration('Piter',
+                               'Parker',
+                               'spiderman@example.com',
+                               'Male',
+                               '1234567890',
+                               28,
+                               'May',
+                               '1995',
+                               'Reading',
+                               './images/111.jpg',
+                               'Vladimir Street 12',
+                               'Haryana',
+                               'Karnal'
+                               )
 
 
 def test_automation_form():
-
     page.open_browser()
-    page.user_registration(Piter)
+    page.user_registration(*user1)
     page.submit()
-    page.should_user().should(have.exact_texts(*Piter))
+    page.should_user().should(have.exact_texts(*user1))
 
 
 
